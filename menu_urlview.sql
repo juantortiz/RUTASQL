8831adead5df9f7cbc37cab3f2bdfef0e604895b
Juan Tomas Ortiz <juantortiz@gmail.com>
2019-08-16 15:27:36 -0300

    Upgrade de mnu_url_view

--- menu_urlview.sql
+++ menu_urlview.sql
@@ -1,9 +1,11 @@
-CREATE OR REPLACE VIEW public.mnu_urlview
-AS SELECT mnu.mnu_url AS url
-   FROM mnu
-  ORDER BY mnu.mnuid;
+CREATE OR REPLACE VIEW public.mnu_url_view
+AS SELECT mnu_url.id,
+    mnu_url.url,
+    mnu_url.descripcion
+   FROM mnu_url
+  ORDER BY mnu_url.id;
 
 -- Permissions
 
-ALTER TABLE public.mnu_urlview OWNER TO postgres;
-GRANT ALL ON TABLE public.mnu_urlview TO postgres;
+ALTER TABLE public.mnu_url_view OWNER TO postgres;
+GRANT ALL ON TABLE public.mnu_url_view TO postgres;
